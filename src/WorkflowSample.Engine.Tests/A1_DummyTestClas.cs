4e27ed0973ca0ea70a7b123837df46f776d22591
Mark Whitfeld <mark.whitfeld@derivco.co.za>
2016-11-17 11:09:39 +0200

    test: Added dummy test class to absorb test runner and JIT compilation warmup in test timings

---
+++ src/WorkflowSample.Engine.Tests/A1_DummyTestClas.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace WorkflowSample.Engine.Tests
+{
+    [TestFixture]
+    public class A1_DummyTestClas 
+    {
+        [Test]
+        public void DummyTestToAbsorbTestRunnerAndJitCompileWarmup()
+        {
+            new TravelRequest();
+            new TravelRequestWorkflow_Simple();
+            new TravelRequestWorkflow_ReusableWithConfigurators();
+            new TravelRequestWorkflow_SimpleWithConfigurators(new StateMachineFactory(new TravelRequestWorkflowGeneralConfigurator()));
+            Assert.Pass();
+        }
+    }
+}
